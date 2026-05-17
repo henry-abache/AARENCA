df38118860694f52e26c96caadc0ab287fc119fe
henry-abache <henryabache@gmail.com>
2026-05-17 12:33:26 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,26 @@
-const CACHE_NAME = 'nueva-vida-aa-v2';
-const urlsToCache = [
+const CACHE_NAME = 'nuevavida-v1';
+const ASSETS = [
   './',
-  './index.html',
-  './manifest.json'
+  './index.html'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+// Instalar el Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
+// Activar el Service Worker
+self.addEventListener('activate', (e) => {
+  console.log('Service Worker activo');
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+// Interceptar peticiones para que funcione offline básico
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
