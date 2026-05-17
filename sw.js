fafef70986fe25a74b708dc1bcaa61172bae7dc5
henry-abache <henryabache@gmail.com>
2026-05-17 13:37:13 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-
+const mapUrl = ev.latitud ? `https://www.google.com/maps/dir/?api=1&destination=${ev.latitud},${ev.longitud}` : `https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(ev.lugar)}`;
