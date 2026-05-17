c161320ab08afa04c4671fbd76c2a54a9aeaa66a
henry-abache <henryabache@gmail.com>
2026-05-17 09:03:20 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
 const CACHE_NAME = 'nueva-vida-aa-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const urlsToCache = ['index.html', 'manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
