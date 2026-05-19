edabe369bc1f391d5ab4e4e1201b72048514ce43
henry-abache <henryabache@gmail.com>
2026-05-18 23:08:52 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,29 @@
-const CACHE = 'nv-aa-v3';
-const ARCH = ['./index.html', './manifest.json', './1-192.png', './1-512.png', './logo-aa.png'];
+const CACHE_NAME = 'nueva-vida-aa-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './1-192.png',
+  './1-512.png'
+];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCH).catch(() => {})));
-    self.skipWaiting();
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-    e.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-        )
-    );
-    self.clients.claim();
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(r => {
-            return r || fetch(e.request).then(res => {
-                const copia = res.clone();
-                caches.open(CACHE).then(c => c.put(e.request, copia));
-                return res;
-            }).catch(() => caches.match('./index.html'));
-        })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
